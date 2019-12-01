3dcfee505df130e856ef2246da4888605779f6c5
guoqiangli <lee.guoqiang@gmail.com>
2019-12-01 23:23:36 +0100

    first upload

---
+++ src/main/java/com/testing/packC/Mainprogram.java
@@ -0,0 +1,18 @@
+package com.testing.packC;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class Mainprogram {
+	
+	private static final Logger LOGGER =
+		      LoggerFactory.getLogger(Mainprogram.class);
+	void firstFun()
+	{
+	      LOGGER.info("Content getFile");
+
+	}
+
+}
